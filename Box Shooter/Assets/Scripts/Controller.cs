078375675697a7c7f9b9ecbf2fd5b5e2b708c2a4
Harish Nair <nairharish1999@gmail.com>
2020-05-27 05:34:18 +0530

    player env setup

--- Box Shooter/Assets/Scripts/Controller.cs
+++ Box Shooter/Assets/Scripts/Controller.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6006a1090621bec38423f51cbb61bdfe168036def218d6b2a54b4574a83a03b
-size 1388
+oid sha256:b4f930aabef20fde15265b6521c3c3007225d5a938e9be494f50795f639a9abc
+size 2651
